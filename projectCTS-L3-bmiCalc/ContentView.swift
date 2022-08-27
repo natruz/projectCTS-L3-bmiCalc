ef56110aecdc7e0fffdd80a4d77d3ebd7f8c5ad6
natruz <88256962+natruz@users.noreply.github.com>
2022-08-27 09:49:37 +0800

    Background, TextFields and Texts

--- projectCTS-L3-bmiCalc/ContentView.swift
+++ projectCTS-L3-bmiCalc/ContentView.swift
@@ -8,14 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var height: String = ""
+    @State var weight: String = ""
+    
+    let textBoxColour = Color(red: 143/255, green: 161/255, blue: 139/255)
+    let textColour = Color(red: 100/255, green: 100/255, blue: 100/255)
+    
     var body: some View {
-        Text("Hello, world!")
+        ZStack {
+            Image("ICT-L3-photo1")
+                .resizable()
+                .ignoresSafeArea()
+            VStack {
+                TextField("Height", text: $height)
+                    .foregroundColor(textColour)
+                    .padding()
+                    .background(textBoxColour)
+                    .cornerRadius(15)
+                TextField("Weight", text: $weight)
+                    .foregroundColor(textColour)
+                    .padding()
+                    .background(textBoxColour)
+                    .cornerRadius(15)
+                Text("BMI: ")
+                    .foregroundColor(textColour)
+                    .padding()
+                    .background(textBoxColour)
+                    .cornerRadius(15)
+                Text("Health Risk: ")
+                    .foregroundColor(textColour)
+                    .padding()
+                    .background(textBoxColour)
+                    .cornerRadius(15)
+            }
             .padding()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(height: "1.5", weight: "50")
     }
 }
