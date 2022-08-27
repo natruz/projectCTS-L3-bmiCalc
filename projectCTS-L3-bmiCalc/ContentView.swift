fc74ea41adc55076f2aabcca3b9eada5381f1a8a
natruz <88256962+natruz@users.noreply.github.com>
2022-08-27 09:24:49 +0800

    Initial Commit

---
+++ projectCTS-L3-bmiCalc/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  projectCTS-L3-bmiCalc
+//
+//  Created by NATALIE CHEN HUI REGINA RUZSICSK stu on 27/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ projectCTS-L3-bmiCalc/projectCTS_L3_bmiCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  projectCTS_L3_bmiCalcApp.swift
+//  projectCTS-L3-bmiCalc
+//
+//  Created by NATALIE CHEN HUI REGINA RUZSICSK stu on 27/8/22.
+//
+
+import SwiftUI
+
+@main
+struct projectCTS_L3_bmiCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
