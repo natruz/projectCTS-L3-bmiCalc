dd6b6276963de1dbe543db0fffb3b17bed9f7d3e
natruz <88256962+natruz@users.noreply.github.com>
2022-08-29 11:25:16 +0800

    Completed slider w/ live updates

--- projectCTS-L3-bmiCalc/ColourSliderView.swift
+++ projectCTS-L3-bmiCalc/ColourSliderView.swift
@@ -8,8 +8,26 @@
 import SwiftUI
 
 struct ColourSliderView: View {
+    
+    @State var sliderR = 50.0
+    @State var sliderG = 50.0
+    @State var sliderB = 50.0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color(red: sliderR / 255,
+                  green: sliderG / 255,
+                  blue: sliderB / 255)
+            .ignoresSafeArea()
+            VStack {
+                Slider(value: $sliderR, in: 0...255)
+                    .padding(30)
+                Slider(value: $sliderG, in: 0...255)
+                    .padding(30)
+                Slider(value: $sliderB, in: 0...255)
+                    .padding(30)
+            }
+        }
     }
 }
 
